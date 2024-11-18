9948241047085804f27880e43053f9a2dededb98
Mr. Tong <15652952678@163.com>
2024-11-18 23:33:22 +0800

    first commit

---
+++ uno.config.ts
@@ -0,0 +1,12 @@
+// Copyright (c) 2024 tong<admin>
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+// uno.config.ts
+import { defineConfig } from 'unocss'
+import presetWind from '@unocss/preset-wind'
+
+export default defineConfig({
+  presets: [presetWind()],
+})
